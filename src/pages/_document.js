6862523d12ff74be99298fbc15552ce035d63799
weiyolan <yolan.weiler@gmail.com>
2023-10-10 19:37:05 +0200

    new commit

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import { useRouter } from "next/router";
 
 export default function Document() {
+  let { locale } = useRouter();
+
   return (
-    <Html lang="en">
+    <Html lang={locale}>
       <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
