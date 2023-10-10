d54eec8b6603596db3506ee1070c8ad06f67ccbe
weiyolan <yolan.weiler@gmail.com>
2023-10-10 19:57:17 +0200

    new commit

--- src/utils/useLocale.jsx
+++ src/utils/useLocale.jsx
@@ -4,7 +4,7 @@ import { useRouter } from "next/router";
 
 export default function useLocale() {
   const { locale } = useRouter();
-  return locale;
+  return locale
 }
 
 // export function useLocale() {
